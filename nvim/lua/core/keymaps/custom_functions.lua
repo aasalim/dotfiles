524f810276ca3c621e4fbdc9373aa7ff2382c7c9
Ahmed Salim <ahmedslim12@gmail.com>
2025-01-28 07:26:27 -0500

    fix:

--- nvim/lua/core/keymaps/custom_functions.lua
+++ nvim/lua/core/keymaps/custom_functions.lua
@@ -61,3 +61,23 @@ local function reload()
 end
 vim.keymap.set("n", "<leader>ve", ":tabnew $VIMRC<cr>", { desc = "Open neovim configuration file" })
 vim.keymap.set("n", "<leader>r", reload, { desc = "Re-source neovim configuration file" })
+
+-- Map <Leader>diary to call WikiDiaryTemplate
+vim.api.nvim_set_keymap('n', '<Leader>diary', ':lua WikiDiaryTemplate()<CR>', { noremap = true, silent = true })
+
+-- Function to insert a diary template
+function WikiDiaryTemplate()
+  local template = {
+    '# ' .. os.date("%Y-%m-%d"),
+    '',
+    '## TODO',
+    '* [ ] ',
+    '',
+    '## NOTES',
+    '- ',
+  }
+
+  -- Replace the current line with the template
+  vim.api.nvim_buf_set_lines(0, vim.fn.line('.') - 1, vim.fn.line('.'), false, template)
+end
+
